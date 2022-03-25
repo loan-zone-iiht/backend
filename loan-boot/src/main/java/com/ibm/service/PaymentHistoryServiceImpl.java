ec412973fceb07dc816bae04216c02acd7e05b86
sayak <sayak.94.sm@gmail.com>
2022-03-25 14:17:01 +0530

    Payment History ServiceImpl Created

---
+++ loan-boot/src/main/java/com/ibm/service/PaymentHistoryServiceImpl.java
@@ -0,0 +1,39 @@
+package com.ibm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibm.entity.PaymentHistory;
+import com.ibm.exception.GlobalLoanException;
+import com.ibm.repo.PaymentHistoryRepository;
+
+@Service
+public class PaymentHistoryServiceImpl implements PaymentHistoryService {
+
+	@Autowired
+	private PaymentHistoryRepository paymentHistoryRepo;
+
+	@Override
+	public PaymentHistory createPaymentHistory(PaymentHistory ph) {
+		return paymentHistoryRepo.save(ph);
+	}
+
+	@Override
+	public PaymentHistory getPaymentHistoryById(int phId) {
+		return paymentHistoryRepo.findById(phId)
+				.orElseThrow(() -> (new GlobalLoanException("404", "No paymentHistory with this id")));
+	}
+
+	@Override
+	public List<PaymentHistory> findAllByCustomerId(int custId) {
+		return paymentHistoryRepo.findAllByCustomerId(custId);
+	}
+
+	@Override
+	public List<PaymentHistory> findAllByLoanDetailsLoanId(int loanId) {
+		return paymentHistoryRepo.findAllByLoanDetailsLoanId(loanId);
+	}
+
+}
