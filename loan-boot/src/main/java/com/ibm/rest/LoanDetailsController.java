f2d0fa8a1435b59ff413da2a51ce8fd93519c7f5
SD170 <duttasaswata7@gmail.com>
2022-04-06 14:26:15 +0530

    CORS added in controllers

--- loan-boot/src/main/java/com/ibm/rest/LoanDetailsController.java
+++ loan-boot/src/main/java/com/ibm/rest/LoanDetailsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,8 @@ import com.ibm.service.LoanDetailsService;
  * 
  * @author Saswata Dutta
  */
+
+@CrossOrigin
 @RestController
 public class LoanDetailsController {
 	@Autowired
--- loan-boot/src/main/java/com/ibm/rest/ManagerController.java
+++ loan-boot/src/main/java/com/ibm/rest/ManagerController.java
@@ -3,6 +3,7 @@ package com.ibm.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,8 @@ import com.ibm.service.ManagerService;
  * 
  * @author Saswata Dutta
  */
+
+@CrossOrigin
 @RestController
 public class ManagerController {
 	@Autowired
