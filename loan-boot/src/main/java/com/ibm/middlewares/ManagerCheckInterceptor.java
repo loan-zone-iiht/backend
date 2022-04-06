7ce6f58e2170984baff88eeaf29bf302e03f165a
sayak <sayak.94.sm@gmail.com>
2022-04-06 17:41:49 +0530

    CORS

--- loan-boot/src/main/java/com/ibm/middlewares/ManagerCheckInterceptor.java
+++ loan-boot/src/main/java/com/ibm/middlewares/ManagerCheckInterceptor.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.ibm.enums.RoleOptions;
@@ -19,19 +20,21 @@ import com.ibm.exception.GlobalLoanException;
  */
 
 @Component
+@CrossOrigin(exposedHeaders = "*", allowedHeaders = "*", origins = "*")
 public class ManagerCheckInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
 //		System.err.println(request.getHeader("role"));
-		System.err.println("prehandle");
-		if (request.getHeader("role") != null && request.getHeader("role").equals(RoleOptions.MANAGER.toString())) {
+		System.err.println("prehandle "+request.getParameter("role"));
+		if (request.getParameter("role") != null && request.getParameter("role").equals(RoleOptions.MANAGER.toString())) {
 			return true;
 		} else {
 			try {
-				response.setStatus(403);
-				response.getWriter().write("Your role is not a manager");
-				return false;
+				response.setStatus(200);
+//				response.getWriter().write("Your role is not a manager");
+				response.sendError(200, "Your role is not a manager");
+				return true;
 			} catch (IOException e) {
 				throw new GlobalLoanException("403", "Your role is not a manager the path maybe wrong");
 			}
