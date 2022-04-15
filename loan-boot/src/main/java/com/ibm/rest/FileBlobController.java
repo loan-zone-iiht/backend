6e7fe90596c569b4d160f6068c44c94a8f1ce069
SD170 <duttasaswata7@gmail.com>
2022-04-15 11:09:09 +0530

    Feature: Supporting document upload for customer
    Customers can upload documents (doc/pdf) of supporting document, for
    managers to review.

--- loan-boot/src/main/java/com/ibm/rest/FileBlobController.java
+++ loan-boot/src/main/java/com/ibm/rest/FileBlobController.java
@@ -32,8 +32,6 @@ import com.ibm.service.FileBlobService;
 public class FileBlobController {
 	@Autowired
 	private FileBlobService fileBlobService;
-	@Autowired
-	private CustomerService customerService;
 	private ResponseHeader rh;
 
 	@PostMapping(path = "/upload-profile-pic")
@@ -59,5 +57,19 @@ public class FileBlobController {
 				HttpStatus.OK);
 		return res;
 
+	}
+	
+	@PostMapping(path = "/upload-suporting-doc")
+	public ResponseEntity<FileBlob> uploadSupportingDoc(@RequestBody MultipartFile file, @RequestParam int custId) {
+		try {
+//			return fileBlobService.saveProfilePic(custId, file);
+			rh = new ResponseHeader();
+			rh.putOnMap("success", "true");
+			ResponseEntity<FileBlob> res = new ResponseEntity<FileBlob>(fileBlobService.saveSupprtingDoc(custId, file),
+					rh.getHeaders(), HttpStatus.OK);
+			return res;
+		} catch (IllegalArgumentException e) {
+			throw new GlobalLoanException("400", "Error uploading file/ try reselecting the file" + e.getMessage());
+		}
 	}
 }
